9405238d425be5f146d1fcddcb2b479b0c994315
rpc08002 <rcurtin0491@gmail.com>
2021-01-21 23:00:38 -0500

    added server js file, installed express

---
+++ routes/apiRoutes.js
@@ -0,0 +1,9 @@
+var noteData = require ("../db/db.json");
+
+module.exports = function (app) {
+    // class Store {
+    //     read() {
+    //       return readFileAsync('db/db.json', 'utf8');
+    //     }
+    // } PROVIDED BY LEARNING BCS REVIEW *****
+}
