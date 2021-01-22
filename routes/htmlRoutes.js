8fc67fcb4c241f7f4aeb8424753fa1d24e6ff509
rpc08002 <rcurtin0491@gmail.com>
2021-01-21 22:27:09 -0500

    created html routes for html pages

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+      });
+
+      app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+
+      });
+
+    };
+
+    
