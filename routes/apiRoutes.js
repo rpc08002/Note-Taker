c8dacbad38afa1a9bcbec6a79f25abf53d360cf3
rpc08002 <rcurtin0491@gmail.com>
2021-01-26 17:30:45 -0500

    added notes from tutor and learning BCS within server.js and route files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,35 @@
-var noteData = require ("../db/db.json");
 
-module.exports = function (app) {
-    // class Store {
-    //     read() {
-    //       return readFileAsync('db/db.json', 'utf8');
-    //     }
-    // } PROVIDED BY LEARNING BCS REVIEW *****
-}
+
+// on post route you have to:
+// 1. read in json file (comaes in as stringified)
+// 2. after file is read turn json into js-array
+// 3. once turned into js -array push new note to array
+// 4. stringify data again
+// 5. resave that data back to the json file
+// module.exports = function (app) {
+//     class Store {
+//         read() {
+//           return readFileAsync('db/db.json', 'utf8');
+//         }
+//     }
+// }
+
+/* 
+  const newNote = req.body;
+  fs.readfile("db.json","utf-8",(err,data)=>{
+  if(err) console.log(err)
+  const parsedd_data = JSON.parse(data);
+  const notes = parsed_data.notes
+  notes.push(newNote)
+  fs.writefile("db.json", jSON.stringify(notes),(err)=>{
+    if(err) console.log(err)
+    return res
+    .status(201)
+    .json({
+      notes:notes,
+      msg:"note added"
+    })
+  })
+})
+
+*/
