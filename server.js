55f744f6e0b0be022ea18b33fc77040821f26ca5
rpc08002 <rcurtin0491@gmail.com>
2021-01-26 17:46:22 -0500

    required fs in server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var express = require("express");
+const express = require("express");
+const fs = require("fs");
 
 var app = express();
 
@@ -6,10 +7,11 @@ var PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/assets", express.static("./assets"));
 
-require("./routes/apiRoutes.js")(app);
-require("./routes/htmlRoutes.js")(app);
+require("./routing/html-routes")(app);
+require("./routing/api-routes")(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
+  console.log("App listening on PORT " + PORT);
+});
